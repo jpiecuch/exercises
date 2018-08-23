bc27b86254a8890ebcc8f0f77bab3ec6b6c3760a
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-08-23 21:41:47 +0200

    Refactor AuthProvider

--- src/main/java/pl/jakubpiecuch/exercises/http/auth/BearerAuthProvider.java
+++ src/main/java/pl/jakubpiecuch/exercises/http/auth/BearerAuthProvider.java
@@ -1,12 +1,15 @@
 package pl.jakubpiecuch.exercises.http.auth;
 
 import com.google.inject.Inject;
+import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.*;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.auth.User;
 import io.vertx.ext.web.client.WebClient;
+
+import java.util.Base64;
 
 public class BearerAuthProvider implements AuthProvider {
 
@@ -21,9 +24,10 @@ public class BearerAuthProvider implements AuthProvider {
     public void authenticate(JsonObject authInfo, Handler<AsyncResult<User>> resultHandler) {
 
         MultiMap body = MultiMap.caseInsensitiveMultiMap().add("token", authInfo.getString("token"));
+        JsonObject config = vertx.getOrCreateContext().config();
 
-        WebClient.create(vertx).post(8085, "localhost", "/oauth/check_token")
-                .putHeader(HttpHeaders.AUTHORIZATION.toString(), "Basic Z3ltLWhvbWU6MDU2NGY3YTItMDVkZi00Y2Y5LWJiODUtYmQ0MDU4MjgzNTUz")
+        WebClient.create(vertx).postAbs(config.getJsonObject("oauth2").getString("checkTokenUri"))
+                .putHeader(HttpHeaders.AUTHORIZATION.toString(), getBasicAuth(config))
                 .sendForm(body,res -> {
                     if (res.succeeded()) {
                         resultHandler.handle(Future.succeededFuture(new BearerUser(res.result().bodyAsJsonObject())));
@@ -31,5 +35,10 @@ public class BearerAuthProvider implements AuthProvider {
                         resultHandler.handle(Future.failedFuture("Something bad happened"));
                     }
                 } );
+    }
+
+    private String getBasicAuth(JsonObject config) {
+        JsonObject client = config.getJsonObject("oauth2").getJsonObject("client");
+        return "Basic " + Base64.getEncoder().encodeToString(String.format("%s:%s", client.getString("clientId"), client.getString("clientSecret")).getBytes());
     }
 }
