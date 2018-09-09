7717edbfcea809ecc5d23586608d576207ff0135
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-09-09 19:13:48 +0200

    Support JSON format of Spring Config Server

--- src/main/java/pl/jakubpiecuch/exercises/StarterVerticle.java
+++ src/main/java/pl/jakubpiecuch/exercises/StarterVerticle.java
@@ -4,29 +4,24 @@ package pl.jakubpiecuch.exercises;
 import io.reactivex.Observable;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.AbstractVerticle;
 import lombok.extern.slf4j.Slf4j;
-import pl.jakubpiecuch.exercises.config.provider.ConfigProvider;
-import pl.jakubpiecuch.exercises.config.provider.JsonConfigProvider;
 import pl.jakubpiecuch.exercises.config.retrieveroptions.ChainedConfigRetrieverOptionsProvider;
 import pl.jakubpiecuch.exercises.config.retrieveroptions.ConfigRetrieverOptionsProvider;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class StarterVerticle extends AbstractVerticle {
 
-    private static final String HTTP_VERTICLE = "http";
-    private static final String SERVICE_VERTICLE = "service";
     private final ConfigRetrieverOptionsProvider configRetrieverOptionsProvider;
-    private final ConfigProvider configProvider;
 
     public StarterVerticle() {
         this.configRetrieverOptionsProvider = new ChainedConfigRetrieverOptionsProvider();
-        this.configProvider = new JsonConfigProvider();
     }
 
     @Override
@@ -38,17 +33,16 @@ public class StarterVerticle extends AbstractVerticle {
     private void handle(Future<Void> startFuture, AsyncResult<JsonObject> result) {
         if (result.succeeded()) {
             //expected result is json representation of configuration
-            JsonObject rootConfig = result.result();
+            JsonObject config = result.result();
 
-            getMergedObservable(rootConfig)
-            .subscribe(onNext -> log.info("Verticle {} successfully started", onNext),
+            getMergedObservable(config)
+            .subscribe(onNext -> log.info("Verticle with deployment ID: {} successfully deployed", onNext),
                     error -> startFuture.fail(error.getCause()),
                     () -> {
                 startFuture.complete();
                 log.info("Start procedure completed");
             });
         } else {
-
             startFuture.fail(result.cause());
             vertx.close();
         }
@@ -71,18 +65,20 @@ public class StarterVerticle extends AbstractVerticle {
      * @return list of observable objects
      */
     private List<Observable<String>> getVerticlesToDeploy(JsonObject config) {
-        return Arrays.asList(getVerticleObservable(config, HTTP_VERTICLE),
-                getVerticleObservable(config, SERVICE_VERTICLE));
+        return config.getJsonArray("verticles")
+                .stream()
+                .map(o -> getVerticleObservable((JsonObject) o))
+                .collect(Collectors.toList());
     }
 
     /**
      * Returns Observable for verticle that needs to be deployed
-     * @param config - contains information for deployment
-     * @param name - class name of verticle to deploy
+     * @param config - contains information for verticle deployment
      * @return observable object
      */
-    private Observable<String> getVerticleObservable(JsonObject config, String name) {
-        return vertx.rxDeployVerticle(this.configProvider.getVerticleName(config, name),
-                this.configProvider.getVerticleOptions(config, name)).toObservable();
+    private Observable<String> getVerticleObservable(JsonObject config) {
+        String name = config.getString("name");
+        JsonObject options = config.getJsonObject("options");
+        return vertx.rxDeployVerticle(name, new DeploymentOptions(options)).toObservable();
     }
 }
--- src/main/java/pl/jakubpiecuch/exercises/config/provider/ConfigProvider.java
+++
@@ -1,9 +0,0 @@
-package pl.jakubpiecuch.exercises.config.provider;
-
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.json.JsonObject;
-
-public interface ConfigProvider {
-    DeploymentOptions getVerticleOptions(JsonObject root, String name);
-    String getVerticleName(JsonObject root, String name);
-}
